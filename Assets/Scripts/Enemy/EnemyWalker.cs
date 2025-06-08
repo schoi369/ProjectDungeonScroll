d1554870ee70a2dc39eb856a4606286a295bf396
schoi369 <inspirationchoi@gmail.com>
2025-06-08 23:19:23 +0900

    EnemyWalker의 리팩토링.

--- Assets/Scripts/Enemy/EnemyWalker.cs
+++ Assets/Scripts/Enemy/EnemyWalker.cs
@@ -15,48 +15,51 @@ public class EnemyWalker : EnemyBase
 		Vector2Int playerPos = player.CellPos;
 		Vector2Int enemyPos = m_cellPos;
 
-		// 1. 공격 조건 확인: 플레이어와 인접해 있는가?
+		// 공격 조건 확인: 플레이어와 인접해 있는가?
 		int distance = Mathf.Abs(playerPos.x - enemyPos.x) + Mathf.Abs(playerPos.y - enemyPos.y);
-		if (distance == 1)
+
+		bool nextToPlayer = (distance == 1);
+		if (nextToPlayer)
 		{
-			// 인접하면 공격하고 턴 종료
+			// 공격하고 턴 종료
 			player.TakeDamage(1);
-			return;
-		}
-
-		// 2. 이동 조건 확인: 공격하지 않았다면 플레이어 방향으로 이동 시도
-		int dx = playerPos.x - enemyPos.x;
-		int dy = playerPos.y - enemyPos.y;
-
-		Vector2Int horizontalTarget = new Vector2Int(enemyPos.x + (int)Mathf.Sign(dx), enemyPos.y);
-		Vector2Int verticalTarget = new Vector2Int(enemyPos.x, enemyPos.y + (int)Mathf.Sign(dy));
-
-		bool isHorizontalPrimary = Mathf.Abs(dx) > Mathf.Abs(dy);
-
-		if (isHorizontalPrimary)
-		{
-			// 1순위: 수평 이동 시도
-			if (dx != 0 && board.IsCellWalkable(horizontalTarget))
-			{
-				board.MoveObjectOnBoard(this, horizontalTarget);
-			}
-			// 2순위: 수직 이동 시도
-			else if (dy != 0 && board.IsCellWalkable(verticalTarget))
-			{
-				board.MoveObjectOnBoard(this, verticalTarget);
-			}
 		}
 		else
 		{
-			// 1순위: 수직 이동 시도
-			if (dy != 0 && board.IsCellWalkable(verticalTarget))
+			// 이동 조건 확인: 공격하지 않았다면 플레이어 방향으로 이동 시도
+			int dx = playerPos.x - enemyPos.x;
+			int dy = playerPos.y - enemyPos.y;
+
+			Vector2Int horizontalTarget = new(enemyPos.x + (int)Mathf.Sign(dx), enemyPos.y);
+			Vector2Int verticalTarget = new(enemyPos.x, enemyPos.y + (int)Mathf.Sign(dy));
+
+			bool isHorizontalPrimary = Mathf.Abs(dx) > Mathf.Abs(dy);
+
+			if (isHorizontalPrimary)
 			{
-				board.MoveObjectOnBoard(this, verticalTarget);
+				// 1순위: 수평 이동 시도
+				if (dx != 0 && board.IsCellWalkable(horizontalTarget))
+				{
+					board.MoveObjectOnBoard(this, horizontalTarget);
+				}
+				// 2순위: 수직 이동 시도
+				else if (dy != 0 && board.IsCellWalkable(verticalTarget))
+				{
+					board.MoveObjectOnBoard(this, verticalTarget);
+				}
 			}
-			// 2순위: 수평 이동 시도
-			else if (dx != 0 && board.IsCellWalkable(horizontalTarget))
+			else
 			{
-				board.MoveObjectOnBoard(this, horizontalTarget);
+				// 1순위: 수직 이동 시도
+				if (dy != 0 && board.IsCellWalkable(verticalTarget))
+				{
+					board.MoveObjectOnBoard(this, verticalTarget);
+				}
+				// 2순위: 수평 이동 시도
+				else if (dx != 0 && board.IsCellWalkable(horizontalTarget))
+				{
+					board.MoveObjectOnBoard(this, horizontalTarget);
+				}
 			}
 		}
 	}
